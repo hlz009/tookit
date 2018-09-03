b774cd46dd4cc40fff8955307cf33a90bca28d5b
小智 <583172396@qq.com>
2018-09-03 23:05:22 +0800

    修改二叉查找树insert和remove，并列出递归与非递归两种形式

--- src/main/java/com/hu/tookit/algorithm/datastructure/BSearchTree.java
+++ src/main/java/com/hu/tookit/algorithm/datastructure/BSearchTree.java
@@ -106,12 +106,20 @@ public class BSearchTree<T extends Comparable<? super T>> {
 
 	public void insert(T data) {
 		insert(root, data);
+//		root = insert2(root, data);
 	}
 
-	private void insert(BTreeNode<T> node, T data) {
+	/**
+	 * 非递归形式，如果插入数据后，需要平衡，则不能这样写
+	 * 具体参考AVLTree
+	 * @param node
+	 * @param data
+	 * @return
+	 */
+	private BTreeNode<T> insert(BTreeNode<T> node, T data) {
 		if (null == node) {
-			node = new BTreeNode<T>(null, data, null);
-			return;
+			root = new BTreeNode<T>(null, data, null);
+			return root;
 		}
 		BTreeNode<T> current = node;
 		BTreeNode<T> newNode = new BTreeNode<T>(null, data, null);
@@ -123,7 +131,7 @@ public class BSearchTree<T extends Comparable<? super T>> {
 					break;
 				}
 				current = current.getLeft();
-			} else if (compareResult < 0) {
+			} else if (compareResult > 0) {
 				if (current.getRight() == null) {
 					current.setRight(newNode);
 					break;
@@ -133,29 +141,105 @@ public class BSearchTree<T extends Comparable<? super T>> {
 				break;
 			}
 		}
+		return current;
 	}
 
+	// 递归
+	private BTreeNode<T> insert2(BTreeNode<T> node, T data) {
+		if (null == node) {
+			return new BTreeNode<T>(null, data, null);
+		}
+		int compareResult = data.compareTo(node.getElement());
+		if (compareResult < 0) {
+			node.setLeft(insert(node.getLeft(), data));
+		} else if (compareResult > 0) {
+			node.setRight(insert(node.getRight(), data));
+		}
+		return node;
+	}
+	
 	public void remove(T data) {
+//		root = remove2(root, data);
 		remove(root, data);
 	}
 
-	private void remove(BTreeNode<T> node, T data) {
+	// 非递归
+	private BTreeNode<T> remove(BTreeNode<T> node, T data) {
 		if (null == node) {
-			return;
+			return node;
 		}
-		while(node != null) {
-			int compareResult = data.compareTo(node.getElement());
+		BTreeNode<T> current = node;
+		BTreeNode<T> parent = node;
+		while(current != null) {
+			int compareResult = data.compareTo(current.getElement());
 			if (compareResult < 0) {
-				node = node.getLeft();
+				parent = current;
+				current = current.getLeft();
 			} else if (compareResult > 0) {
-				node = node.getRight();
-			} else if (node.getLeft() != null && node.getRight() != null) {
-				T minData = findMin(node.getRight()).getElement();
-				node.setElement(minData);// 替换element
-				remove(node.getRight(), minData);
+				parent = current;
+				current = current.getRight();
+			} else if (current.getLeft() != null && current.getRight() != null) {
+				BTreeNode<T> sub = current.getRight();
+				while(sub.getLeft() != null) {
+					// 类似于findMin
+					parent = sub;
+					sub = sub.getLeft();
+				}
+				current.setElement(sub.getElement());
+				// sub最多只有右子节点（因为是删除右子树最小的节点，该节点不可能有左子节点）
+				if (parent.getLeft() != null) {
+					parent.setLeft(sub.getRight());
+				} else {
+					parent.setRight(sub.getRight());
+				}
 			} else {
-				node = node.getLeft() != null? node.getLeft(): node.getRight();
+				if (parent.getLeft() != null) {
+					parent.setLeft(current.getLeft() != null? current.getLeft(): current.getRight());
+				} else {
+					parent.setRight(current.getLeft() != null? current.getLeft(): current.getRight());
+				}
+				break;
 			}
 		}
+		return parent;
+	}
+
+	// 递归
+	private BTreeNode<T> remove2(BTreeNode<T> node, T data) {
+		if (null == node) {
+			return node;
+		}
+		int compareResult = data.compareTo(node.getElement());
+		if (compareResult < 0) {
+			node.setLeft(remove2(node.getLeft(), data));
+		} else if (compareResult > 0) {
+			node.setRight(remove2(node.getRight(), data));
+		} else if (node.getLeft() != null && node.getRight() != null) {
+			T minData = findMin(node.getRight()).getElement();
+			node.setElement(minData);// 替换element
+			node.setRight(remove2(node.getRight(), minData));
+		} else {
+			node = node.getLeft() != null? node.getLeft(): node.getRight();
+		}
+		return node;
+	}
+
+	public static void main(String[] args) {
+		BSearchTree<Integer> bst = new BSearchTree<Integer>();
+		bst.insert(6);
+		bst.insert(2);
+		bst.insert(1);
+		bst.insert(5);
+		bst.insert(3);
+		bst.insert(4);
+		bst.insert(8);
+		bst.remove(2);
+		System.out.println(bst.contains(2));
+		System.out.println(bst.contains(1));
+//		System.out.println(bst.contains(3));
+		System.out.println(bst.root.getElement());
+		System.out.println(bst.root.getLeft().getRight().getLeft().getElement());
+//		System.out.println(bst.root.getLeft().getLeft().getElement());
+		System.out.println(bst.root.getRight().getElement());
 	}
 }
