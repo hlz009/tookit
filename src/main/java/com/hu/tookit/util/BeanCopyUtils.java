152cfd4b1da08fbb306c2f496fc47474c8af7c6e
小智 <583172396@qq.com>
2019-07-04 10:55:43 +0800

    新增对象复制工具类

---
+++ src/main/java/com/hu/tookit/util/BeanCopyUtils.java
@@ -0,0 +1,36 @@
+package com.hu.tookit.util;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+/**
+ * 忽略复制对象的空属性字段，
+ * 利用了org.springframework.beans.BeanUtils，忽略复制某个字段的方法
+ * @author xiaozhi009
+ *
+ */
+public class BeanCopyUtils {
+	private BeanCopyUtils() {
+	}
+
+	public static String[] getNullPropertyNames (Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<>();
+        for(java.beans.PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) emptyNames.add(pd.getName());
+        }
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
+    }
+
+    public static void copyPropertiesIgnoreNull(Object src, Object target){
+        BeanUtils.copyProperties(src, target, getNullPropertyNames(src));
+    }
+}
